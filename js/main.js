bff9be1531265b82b7343df3cb24217f13defba3
amrme <amr.e.dev@gmail.com>
2016-01-11 12:04:10 +0200

    js minif configured

---
+++ js/main.js
@@ -0,0 +1,3 @@
+$("document").ready(function() {
+  $(".btn").click(function() {alert("It works");});
+});
---
+++ js/min/main-min.js
@@ -0,0 +1,1 @@
+$("document").ready(function(){$(".btn").click(function(){alert("It works")})});
