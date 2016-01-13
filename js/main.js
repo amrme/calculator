3e105fa69c832026eec60886cfeaa9b2d5cf6344
amrme <amr.e.dev@gmail.com>
2016-01-13 09:07:25 +0200

    added comments

--- js/main.js
+++ js/main.js
@@ -7,17 +7,23 @@ $("document").ready(function() {
   // if tesult has 3 elements evaluate
   if(result.length === 3)
   {
+    // evaluate the result and post it to the display
     evaluate();
   }
 
   $(".btn").click(function() {
+    // if you already have pending operations
     if (result.length === 3)
     {
+      // evaluate the result and post it to the display
       evaluate();
     }
+    // if not start a new one
     else
     {
+      // get element's value from it's html tag
       $("#result").html($(this).html());
+      // push item to result
       result.push($(this).html());
       //console.log($(this).html());
     }
@@ -25,10 +31,16 @@ $("document").ready(function() {
 
 });
 
+// evaluates the calculations
 function evaluate() {
+  // store the result of the calculations here
   var currentResult = eval(result.join(''));
+  // put result on display
   $("#result").html(currentResult);
+  // remove the 2nd and 3rd items from result array
   result.splice(1,2);
+  // replace the first element with the reuslt of the previous calculation
   result[0] = currentResult;
-  console.log(result.join(""), result.length);
+
+  //console.log(result.join(""), result.length);
 }
