f6d4b77febb4099c0d55460c06c1d3578264965e
amrme <amr.e.dev@gmail.com>
2016-01-13 08:53:48 +0200

    rewrote it with a different idea, but still needd to pass condition when same operator is pressed again instead of equal

--- js/main.js
+++ js/main.js
@@ -1,115 +1,34 @@
 
 // global variables
-// result is the current result
-var result = 0;
-var prev = 0;
-var operation = null;
-var current = 0;
+var result = [];
 
 $("document").ready(function() {
-  // clear button
-  $("#clear-btn").click(function(){
-    result = 0;
-    prev = 0;
-    current = 0;
-    // post result
-    $("#result").html(result);
-  });
-  // plus
-  $("#add").click(function(){
-    if(operation == null)
+
+  // if tesult has 3 elements evaluate
+  if(result.length === 3)
+  {
+    evaluate();
+  }
+
+  $(".btn").click(function() {
+    if (result.length === 3)
     {
-      operation = "+";
-      prev = current;
-      current = 0;
+      evaluate();
     }
-
-  });
-
-
-  // equals
-  $("#equals").click(function(){
-    // post result
-    switch (operation) {
-      case '+':
-        result = prev + current;
-        operation = null;
-        break;
-      case '-':
-        result = prev - current;
-        operation = null;
-        break;
-      case 'x':
-        result = prev * current;
-        operation = null;
-        break;
-      case '/':
-        result = prev / current;
-        operation = null;
-        break;
-
+    else
+    {
+      $("#result").html($(this).html());
+      result.push($(this).html());
+      //console.log($(this).html());
     }
-    $("#result").html(result);
   });
-  // one
-  $("#one").click(function(){
-    current = Number(String(current) + '1');
-    // post result
-    $("#result").html(current);
-  });
-  // two
-  $("#two").click(function(){
-    current = Number(String(current) + '2');
-    // post result
-    $("#result").html(current);
-  });
-  // three
-  $("#three").click(function(){
-    current = Number(String(current) + '3');
-    // post result
-    $("#result").html(current);
-  });
-  // four
-  $("#four").click(function(){
-    current = Number(String(current) + '4');
-    // post result
-    $("#result").html(current);
-  });
-  // five
-  $("#five").click(function(){
-    current = Number(String(current) + '5');
-    // post result
-    $("#result").html(current);
-  });
-  // six
-  $("#six").click(function(){
-    current = Number(String(current) + '6');
-    // post result
-    $("#result").html(current);
-  });
-  // seven
-  $("#seven").click(function(){
-    current = Number(String(current) + '7');
-    // post result
-    $("#result").html(current);
-  });
-  // eight
-  $("#eight").click(function(){
-    current = Number(String(current) + '8');
-    // post result
-    $("#result").html(current);
-  });
-  // nine
-  $("#nine").click(function(){
-    current = Number(String(current) + '9');
-    // post result
-    $("#result").html(current);
-  });
-  // zero
-  $("#zero").click(function(){
-    current = Number(String(current) + '0');
-    // post result
-    $("#result").html(current);
-  });
-  //
+
 });
+
+function evaluate() {
+  var currentResult = eval(result.join(''));
+  $("#result").html(currentResult);
+  result.splice(1,2);
+  result[0] = currentResult;
+  console.log(result.join(""), result.length);
+}
